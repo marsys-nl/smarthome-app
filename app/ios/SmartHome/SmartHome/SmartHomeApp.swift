b9d04575d33852ffc54897a772d154e9f4c636cd
Niels Marsman <niels.marsman@gmail.com>
2026-01-12 21:22:16 +0100

    SMART-15 - Initialized xcode project, created workspace, adjusted gradle to make it work

---
+++ app/ios/SmartHome/SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Niels Marsman on 11/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app/library/src/iosMain/kotlin/network/smarthome/app/library/Platform.kt
@@ -0,0 +1,7 @@
+package network.smarthome.app.library
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun mainViewController() = ComposeUIViewController {
+    App()
+}
