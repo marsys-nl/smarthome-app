4a1797e56018b3dd09ed8f0fe2fab8be87e60e76
Niels Marsman <niels.marsman@gmail.com>
2026-02-09 13:58:18 +0100

    SMART-21 - Introduced object to keep track of switch specific tokens

---
+++ shared/library/design/src/commonMain/kotlin/network/marsys/smarthome/shared/library/design/theme/tokens/components/SwitchTokens.kt
@@ -0,0 +1,15 @@
+package network.marsys.smarthome.shared.library.design.theme.tokens.components
+
+import androidx.compose.ui.graphics.Color
+import network.marsys.smarthome.shared.library.design.theme.tokens.PaletteTokens
+
+object SwitchTokens {
+    val CheckedTrackColor: Color = PaletteTokens.Emerald.Emerald500
+    val UncheckedTrackColor: Color = PaletteTokens.Neutral.Neutral200
+    val CheckedThumbColor: Color = PaletteTokens.Base.White
+    val UncheckedThumbColor: Color = PaletteTokens.Base.White
+    val DisabledCheckedTrackColor: Color = PaletteTokens.Neutral.Neutral100
+    val DisabledUncheckedTrackColor: Color = PaletteTokens.Neutral.Neutral100
+    val DisabledCheckedThumbColor: Color = PaletteTokens.Base.White
+    val DisabledUncheckedThumbColor: Color = PaletteTokens.Base.White
+}
