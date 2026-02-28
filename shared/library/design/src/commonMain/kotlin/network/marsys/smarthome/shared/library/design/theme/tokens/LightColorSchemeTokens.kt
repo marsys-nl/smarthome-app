3e129296e5c383013aa9cebe547dfa46af995007
Niels Marsman <niels.marsman@gmail.com>
2026-02-28 09:37:51 +0100

    SMART-21 - Introduced secondary text color

--- shared/library/design/src/commonMain/kotlin/network/marsys/smarthome/shared/library/design/theme/tokens/LightColorSchemeTokens.kt
+++ shared/library/design/src/commonMain/kotlin/network/marsys/smarthome/shared/library/design/theme/tokens/LightColorSchemeTokens.kt
@@ -15,5 +15,6 @@ internal object LightColorSchemeTokens : ColorSchemeTokens {
     override val ForegroundBrandPrimary: Color = PaletteTokens.Amber.Amber500
     override val ForegroundDisabled: Color = PaletteTokens.Slate.Slate50
     override val TextPrimary: Color = PaletteTokens.Slate.Slate800
+    override val TextSecondary: Color = PaletteTokens.Slate.Slate500
     override val TextDisabled: Color = PaletteTokens.Slate.Slate400
 }
