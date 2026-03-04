e10853f69800b854134186b9cb09ca639d5e7629
Niels Marsman <niels.marsman@gmail.com>
2026-03-04 16:06:25 +0100

    SMART-21 - Removed unused imports

--- shared/feature/onboarding/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/onboarding/components/OnboardingScreenScaffold.kt
+++ shared/feature/onboarding/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/onboarding/components/OnboardingScreenScaffold.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
