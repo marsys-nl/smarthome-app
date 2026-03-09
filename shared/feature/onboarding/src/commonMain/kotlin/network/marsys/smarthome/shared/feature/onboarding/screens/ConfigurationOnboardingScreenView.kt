275ff1793e5d420d5cd05a321c746308f6a4d9aa
Niels Marsman <niels.marsman@gmail.com>
2026-03-09 13:25:01 +0100

    SMART-21 - Initial setup of the onboarding configuration screen

---
+++ shared/feature/onboarding/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/onboarding/screens/ConfigurationOnboardingScreenView.kt
@@ -0,0 +1,145 @@
+package network.marsys.smarthome.shared.feature.onboarding.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import network.marsys.smarthome.shared.feature.onboarding.OnboardingScreens
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingBackButton
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingNextButton
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingProgressIndicator
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingProgressIndicatorDefaults
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingScreenIcon
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingScreenIndicator
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingScreenIndicatorDefaults
+import network.marsys.smarthome.shared.feature.onboarding.components.OnboardingScreenScaffold
+import network.marsys.smarthome.shared.feature.onboarding.onboarding.generated.resources.Res
+import network.marsys.smarthome.shared.feature.onboarding.onboarding.generated.resources.onboarding_configuration_subtitle
+import network.marsys.smarthome.shared.feature.onboarding.onboarding.generated.resources.onboarding_configuration_title
+import network.marsys.smarthome.shared.library.design.SmartHomeTheme
+import network.marsys.smarthome.shared.library.design.ThemeSelection
+import network.marsys.smarthome.shared.library.design.annotation.PreviewFontScales
+import network.marsys.smarthome.shared.library.design.annotation.PreviewLocales
+import network.marsys.smarthome.shared.library.design.annotation.PreviewScreenSizes
+import network.marsys.smarthome.shared.library.design.component.ButtonDefaults
+import network.marsys.smarthome.shared.library.design.component.Text
+import network.marsys.smarthome.shared.library.design.icons.Icons
+import network.marsys.smarthome.shared.library.design.icons.Server
+import network.marsys.smarthome.shared.library.design.theme.LocalColorScheme
+import network.marsys.smarthome.shared.library.design.theme.ThemeSelectionPreviewParameterProvider
+import network.marsys.smarthome.shared.library.design.theme.tokens.GradientKeyToken
+import network.marsys.smarthome.shared.library.design.theme.tokens.PaletteTokens
+import org.jetbrains.compose.resources.stringResource
+
+private val BrandPrimaryToSecondaryGradient
+    @Composable
+    get() = LocalColorScheme.current[GradientKeyToken.BrandPrimaryToSecondary]
+
+@Composable
+@Suppress("LongMethod")
+fun ConfigurationOnboardingScreenView(
+    // validating: Boolean,
+    finishOnboarding: () -> Unit,
+    // skipToDemo: () -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier.Companion,
+) {
+    val numberOfScreens: Int = OnboardingScreens.SCREEN_COUNT
+    val indexOfScreen: Int = OnboardingScreens.indexOf(OnboardingScreens.Configuration)
+
+    OnboardingScreenScaffold(
+        modifier = modifier,
+        header = {
+            OnboardingProgressIndicator(
+                numberOfScreens = numberOfScreens,
+                indexOfScreen = indexOfScreen,
+                colors = OnboardingProgressIndicatorDefaults.colors(
+                    foreground = BrandPrimaryToSecondaryGradient,
+                ),
+                modifier = Modifier
+                    .padding(bottom = 40.dp),
+            )
+
+            OnboardingScreenIcon(
+                icon = Icons.Server,
+                modifier = Modifier
+                    .padding(bottom = 16.dp),
+            )
+
+            Text(
+                text = stringResource(Res.string.onboarding_configuration_title),
+                modifier = Modifier
+                    .padding(bottom = 8.dp),
+                textAlign = TextAlign.Center,
+                lineHeight = 32.sp,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.W700,
+            )
+
+            Text(
+                text = stringResource(Res.string.onboarding_configuration_subtitle),
+                textAlign = TextAlign.Center,
+                lineHeight = 20.sp,
+                fontSize = 14.sp,
+            )
+        },
+        footer = {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                OnboardingBackButton(
+                    onClick = navigateBack,
+                )
+
+                OnboardingNextButton(
+                    onClick = finishOnboarding,
+                    colors = ButtonDefaults.colors(
+                        backgroundColor = BrandPrimaryToSecondaryGradient,
+                        contentColor = PaletteTokens.Base.White,
+                    ),
+                )
+            }
+
+            OnboardingScreenIndicator(
+                screen = indexOfScreen,
+                screens = numberOfScreens,
+                modifier = Modifier
+                    .padding(top = 24.dp),
+                colors = OnboardingScreenIndicatorDefaults.colors(
+                    activeScreenIndicatorColor = SolidColor(PaletteTokens.Emerald.Emerald500),
+                    currentScreenIndicatorColor = BrandPrimaryToSecondaryGradient,
+                ),
+            )
+        },
+    ) {
+        // Not implemented yet
+    }
+}
+
+@PreviewLocales
+@PreviewFontScales
+@PreviewScreenSizes
+@Composable
+private fun ConfigurationOnboardingScreenPreview(
+    @PreviewParameter(ThemeSelectionPreviewParameterProvider::class) theme: ThemeSelection,
+) {
+    SmartHomeTheme(
+        theme = theme,
+    ) {
+        ConfigurationOnboardingScreenView(
+            // validating = false,
+            finishOnboarding = {},
+            // skipToDemo = {},
+            navigateBack = {},
+        )
+    }
+}
