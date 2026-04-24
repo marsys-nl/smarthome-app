29279cd81b7507b3908f74ae60cc3e4a2ca26baa
Niels Marsman <niels.marsman@gmail.com>
2026-04-24 09:58:21 +0200

    SMART-21 - Introduce test balloon and kotlin expect to write tests and assertions

---
+++ shared/feature/onboarding/src/commonTest/kotlin/network/marsys/smarthome/shared/feature/onboarding/OnboardingViewModelTest.kt
@@ -0,0 +1,12 @@
+package network.marsys.smarthome.shared.feature.onboarding
+
+import de.infix.testBalloon.framework.core.testSuite
+import dev.nmarsman.expect.api.expectThat
+import dev.nmarsman.expect.assertions.isEqualTo
+
+val OnboardingViewModelTest by testSuite {
+    test("some") {
+        expectThat(1)
+            .isEqualTo(1)
+    }
+}
