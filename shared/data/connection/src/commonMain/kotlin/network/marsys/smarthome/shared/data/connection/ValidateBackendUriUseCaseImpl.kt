ff0a8eaa04f3d0a2d6d83c427982110708312711
Niels Marsman <niels.marsman@gmail.com>
2026-05-06 23:47:00 +0200

    SMART-31 - Validating backend uri should be limited in time

--- shared/data/connection/src/commonMain/kotlin/network/marsys/smarthome/shared/data/connection/ValidateBackendUriUseCaseImpl.kt
+++ shared/data/connection/src/commonMain/kotlin/network/marsys/smarthome/shared/data/connection/ValidateBackendUriUseCaseImpl.kt
@@ -2,6 +2,7 @@ package network.marsys.smarthome.shared.data.connection
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.timeout
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.http.HttpStatusCode
@@ -23,6 +24,10 @@ internal class ValidateBackendUriUseCaseImpl(
             .onFailure { return it }
 
         val response = client.get("${url}${HEALTH_ENDPOINT}") {
+            timeout {
+                requestTimeoutMillis = 10_000
+            }
+
             if (!apiKey.isNullOrBlank()) {
                 header(API_KEY_HEADER, apiKey)
             }
