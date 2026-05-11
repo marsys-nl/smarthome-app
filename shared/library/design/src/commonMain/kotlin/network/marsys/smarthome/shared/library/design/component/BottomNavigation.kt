406938ac7182bf717e1d7f9336df11549d818a26
Niels Marsman <niels.marsman@gmail.com>
2026-05-11 13:41:23 +0200

    SMART-32 - Made the bottom navigation preview interactable

--- shared/library/design/src/commonMain/kotlin/network/marsys/smarthome/shared/library/design/component/BottomNavigation.kt
+++ shared/library/design/src/commonMain/kotlin/network/marsys/smarthome/shared/library/design/component/BottomNavigation.kt
@@ -1,13 +1,11 @@
 package network.marsys.smarthome.shared.library.design.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,32 +17,30 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import network.marsys.smarthome.shared.library.design.SmartHomeComponentPreview
 import network.marsys.smarthome.shared.library.design.SmartHomeTheme
 import network.marsys.smarthome.shared.library.design.ThemeSelection
 import network.marsys.smarthome.shared.library.design.annotation.PreviewScreenSizes
 import network.marsys.smarthome.shared.library.design.icons.Grid
 import network.marsys.smarthome.shared.library.design.icons.House
-import network.marsys.smarthome.shared.library.design.icons.HousePlug
 import network.marsys.smarthome.shared.library.design.icons.Icons
 import network.marsys.smarthome.shared.library.design.icons.User
 import network.marsys.smarthome.shared.library.design.icons.Zap
 import network.marsys.smarthome.shared.library.design.theme.ThemeSelectionPreviewParameterProvider
 import network.marsys.smarthome.shared.library.design.theme.tokens.ColorKeyToken
 import network.marsys.smarthome.shared.library.design.theme.tokens.components.BottomNavigationTokens
-import kotlin.collections.set
-import kotlin.reflect.KClass
 
 @Composable
 fun <T : Any> BottomNavigation(
@@ -245,6 +241,8 @@ object BottomNavigationDefaults {
 private fun BottomNavigationPreview(
     @PreviewParameter(ThemeSelectionPreviewParameterProvider::class) theme: ThemeSelection,
 ) {
+    var selectedNavigationItem by remember { mutableStateOf("home") }
+
     SmartHomeTheme(
         theme = theme,
     ) {
@@ -259,8 +257,10 @@ private fun BottomNavigationPreview(
             )
 
             BottomNavigation(
-                selectedNavigationItem = "home",
-                onNavigationItemSelect = { },
+                selectedNavigationItem = selectedNavigationItem,
+                onNavigationItemSelect = {
+                    selectedNavigationItem = it
+                },
             ) {
                 item(
                     screen = "home",
