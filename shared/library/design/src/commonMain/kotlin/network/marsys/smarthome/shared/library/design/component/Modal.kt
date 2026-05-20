116949e14a097e7eecbfd703229447b013e2f1f3
Niels Marsman <niels.marsman@gmail.com>
2026-05-20 10:00:17 +0200

    SMART-37 - Designed and implemented basic modal

---
+++ shared/library/design/src/commonMain/kotlin/network/marsys/smarthome/shared/library/design/component/Modal.kt
@@ -0,0 +1,172 @@
+package network.marsys.smarthome.shared.library.design.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import core.com.composeunstyled.detectTapGestures
+import network.marsys.smarthome.shared.library.design.SmartHomeTheme
+import network.marsys.smarthome.shared.library.design.ThemeSelection
+import network.marsys.smarthome.shared.library.design.annotation.PreviewScreenSizes
+import network.marsys.smarthome.shared.library.design.theme.ThemeSelectionPreviewParameterProvider
+import network.marsys.smarthome.shared.library.design.theme.tokens.components.ModalTokens
+
+@Immutable
+data class ModalProperties(
+    internal val dismissOnClickOutside: Boolean = true,
+)
+
+@Composable
+fun Modal(
+    onDismissRequest: () -> Unit,
+    properties: ModalProperties = ModalProperties(),
+    colors: ModalColors = ModalDefaults.colors(),
+    content: @Composable () -> Unit
+) {
+    ModalOverlay(
+        onDismissRequest = onDismissRequest,
+        properties = properties,
+        colors = colors,
+    ) {
+        ModalContent(
+            colors = colors,
+            content = content,
+        )
+    }
+}
+
+@Composable
+fun ModalOverlay(
+    onDismissRequest: () -> Unit = {},
+    properties: ModalProperties = ModalProperties(),
+    colors: ModalColors = ModalDefaults.colors(),
+    contentPadding: PaddingValues = ModalDefaults.overlayPadding(),
+    content: @Composable () -> Unit
+) {
+    val dismissOnClickOutsideModifier = if (properties.dismissOnClickOutside) {
+        Modifier
+            .clickable(
+                interactionSource = null,
+                indication = null,
+                onClick = onDismissRequest,
+            )
+    } else {
+        Modifier
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(dismissOnClickOutsideModifier)
+            .background(
+                brush = colors.overlayColor,
+            )
+            .padding(contentPadding),
+        contentAlignment = Alignment.Center,
+        content = { content.invoke() },
+    )
+}
+
+@Composable
+fun ModalContent(
+    colors: ModalColors = ModalDefaults.colors(),
+    contentPadding: PaddingValues = ModalDefaults.modalPadding(),
+    modalShape: Shape = ModalDefaults.modalShape(),
+    content: @Composable () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .widthIn(max = 600.dp)
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = null,
+                indication = null,
+                onClick = { /* Consume clicks to prevent dismissing the modal when clicking inside the content */ },
+            )
+            .background(
+                brush = colors.containerColor,
+                shape = modalShape,
+            )
+            .padding(contentPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        content.invoke()
+    }
+}
+
+@Immutable
+@ConsistentCopyVisibility
+data class ModalColors internal constructor(
+    val overlayColor: Brush,
+    val containerColor: Brush,
+    val contentColor: Color,
+    val borderColor: Color,
+)
+
+object ModalDefaults {
+    @Composable
+    fun colors(
+        overlayColor: Brush = ModalTokens.OverlayColor,
+        containerColor: Brush = ModalTokens.ContainerColor,
+        contentColor: Color = ModalTokens.ContentColor,
+        borderColor: Color = ModalTokens.BorderColor,
+    ) = ModalColors(
+        overlayColor = overlayColor,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        borderColor = borderColor,
+    )
+
+    @Composable
+    fun modalPadding(): PaddingValues = PaddingValues(
+        horizontal = ModalTokens.ModalHorizontalPadding,
+        vertical = ModalTokens.ModalVerticalPadding,
+    )
+
+    @Composable
+    fun modalShape(): Shape = ModalTokens.ModalShape
+
+    @Composable
+    fun overlayPadding(): PaddingValues = PaddingValues(
+        horizontal = ModalTokens.OverlayHorizontalPadding,
+        vertical = ModalTokens.OverlayVerticalPadding,
+    )
+}
+
+@PreviewScreenSizes
+@Composable
+private fun ModalPreview(
+    @PreviewParameter(ThemeSelectionPreviewParameterProvider::class) theme: ThemeSelection,
+) {
+    SmartHomeTheme(
+        theme = theme,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Modal(
+                onDismissRequest = { /* No-op for preview */ },
+            ) {
+                Text(text = "This is a modal")
+            }
+        }
+    }
+}
+
