306e3aa3fd48468bcbf82039c86285f34f11796c
Niels Marsman <niels.marsman@gmail.com>
2026-06-01 12:25:55 +0200

    SMART-41 - Implemented the option to adjust the colors for the grouped entity header component

--- shared/feature/dashboard/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/dashboard/sections/controls/GroupedEntityHeader.kt
+++ shared/feature/dashboard/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/dashboard/sections/controls/GroupedEntityHeader.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLocale
 import androidx.compose.ui.text.font.FontWeight
@@ -19,15 +21,19 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import network.marsys.smarthome.shared.library.design.SmartHomeComponentPreview
+import network.marsys.smarthome.shared.library.design.SmartHomeTheme
 import network.marsys.smarthome.shared.library.design.ThemeSelection
 import network.marsys.smarthome.shared.library.design.component.Text
 import network.marsys.smarthome.shared.library.design.theme.ThemeSelectionPreviewParameterProvider
+import network.marsys.smarthome.shared.library.design.theme.tokens.ColorKeyToken
+import network.marsys.smarthome.shared.library.design.theme.tokens.GradientKeyToken
 
 @Composable
 fun GroupedEntityHeader(
     title: String,
     count: Int,
     modifier: Modifier = Modifier,
+    colors: GroupedEntityHeaderColors = GroupedEntityHeaderDefaults.colors(),
 ) {
     Row(
         modifier = modifier,
@@ -39,7 +45,7 @@ fun GroupedEntityHeader(
             modifier = Modifier
                 .clip(CircleShape)
                 .size(24.dp, 6.dp)
-                .background(Color.Green),
+                .background(colors.markerBackgroundColor),
         )
 
         Text(
@@ -49,6 +55,7 @@ fun GroupedEntityHeader(
             fontSize = 12.sp,
             letterSpacing = 1.sp,
             fontWeight = FontWeight.W700,
+            color = colors.textColor,
         )
 
         Text(
@@ -57,6 +64,23 @@ fun GroupedEntityHeader(
             fontSize = 12.sp,
         )
     }
+}
+
+@Immutable
+data class GroupedEntityHeaderColors(
+    val markerBackgroundColor: Brush,
+    val textColor: Color,
+)
+
+object GroupedEntityHeaderDefaults {
+    @Composable
+    fun colors(
+        markerBackgroundColor: Brush = SmartHomeTheme.colors[GradientKeyToken.BrandPrimaryToSecondary],
+        textColor: Color = SmartHomeTheme.colors[ColorKeyToken.TextPrimary],
+    ): GroupedEntityHeaderColors = GroupedEntityHeaderColors(
+        markerBackgroundColor = markerBackgroundColor,
+        textColor = textColor,
+    )
 }
 
 @Preview
