d90c9f7ca2c187e0dba3f790332395d10985e25d
Niels Marsman <niels.marsman@gmail.com>
2026-07-11 09:09:05 +0200

    SMART-33 - Implemented user info header

--- shared/feature/profile/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/profile/ProfileScreenView.kt
+++ shared/feature/profile/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/profile/ProfileScreenView.kt
@@ -2,54 +2,80 @@ package network.marsys.smarthome.shared.feature.profile
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import network.marsys.smarthome.shared.feature.profile.profile.generated.resources.Res
 import network.marsys.smarthome.shared.feature.profile.profile.generated.resources.profile_header
+import network.marsys.smarthome.shared.library.core.coroutines.produceStateWithLifecycle
 import network.marsys.smarthome.shared.library.design.SmartHomeTheme
 import network.marsys.smarthome.shared.library.design.ThemeSelection
 import network.marsys.smarthome.shared.library.design.adaptive.Breakpoints
 import network.marsys.smarthome.shared.library.design.annotation.PreviewFontScales
 import network.marsys.smarthome.shared.library.design.annotation.PreviewLocales
 import network.marsys.smarthome.shared.library.design.annotation.PreviewScreenSizes
+import network.marsys.smarthome.shared.library.design.component.Card
+import network.marsys.smarthome.shared.library.design.component.CardDefaults
+import network.marsys.smarthome.shared.library.design.component.Icon
 import network.marsys.smarthome.shared.library.design.component.Text
+import network.marsys.smarthome.shared.library.design.icons.Icons
+import network.marsys.smarthome.shared.library.design.icons.User
 import network.marsys.smarthome.shared.library.design.theme.ThemeSelectionPreviewParameterProvider
+import network.marsys.smarthome.shared.library.design.theme.tokens.ColorKeyToken
+import network.marsys.smarthome.shared.library.design.theme.tokens.GradientKeyToken
+import network.marsys.smarthome.shared.library.design.theme.tokens.PaletteTokens
 import network.marsys.smarthome.shared.library.i18n.stringResource
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun ProfileScreenView(
     modifier: Modifier = Modifier,
+    viewModel: ProfileViewModel = koinViewModel(),
 ) {
+    val state = viewModel.produceStateWithLifecycle()
+
     ProfileScreenViewContent(
+        name = state.user,
+        email = state.email,
         modifier = modifier,
     )
 }
 
 @Composable
 private fun ProfileScreenViewContent(
+    name: String,
+    email: String,
     modifier: Modifier = Modifier,
 ) {
     Column(
         modifier = modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement
-            .spacedBy(32.dp),
     ) {
         Column(
             modifier = Modifier
                 .widthIn(max = Breakpoints.MEDIUM.dp)
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement
+                .spacedBy(32.dp),
         ) {
             ProfileScreenHeader()
+            ProfileUserInfo(
+                name = name,
+                email = email,
+            )
         }
     }
 }
@@ -60,10 +86,64 @@ private fun ProfileScreenHeader(
 ) {
     Text(
         text = stringResource(Res.string.profile_header),
+        modifier = modifier,
         lineHeight = 32.sp,
         fontSize = 24.sp,
-        modifier = modifier,
+        fontWeight = FontWeight.W700,
     )
+}
+
+@Composable
+private fun ProfileUserInfo(
+    name: String,
+    email: String,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(24.dp),
+    ) {
+        Row(
+            modifier = Modifier,
+            horizontalArrangement = Arrangement
+                .spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Card(
+                modifier = Modifier
+                    .width(64.dp)
+                    .aspectRatio(1f),
+                colors = CardDefaults.colors(
+                    backgroundColor = SmartHomeTheme.colors[GradientKeyToken.BrandPrimaryToSecondary],
+                ),
+            ) {
+                Icon(
+                    icon = Icons.User,
+                    size = 32.dp,
+                    tint = PaletteTokens.Base.White,
+                )
+            }
+
+            Column(
+                modifier = Modifier,
+            ) {
+                Text(
+                    text = name,
+                    lineHeight = 28.sp,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W700,
+                )
+
+                Text(
+                    text = email,
+                    lineHeight = 20.sp,
+                    fontSize = 14.sp,
+                    color = SmartHomeTheme.colors[ColorKeyToken.TextSecondary],
+                )
+            }
+        }
+    }
 }
 
 @PreviewLocales
@@ -76,6 +156,26 @@ private fun ProfileScreenDemoUserPreview(
     SmartHomeTheme(
         theme = theme,
     ) {
-        ProfileScreenView()
+        ProfileScreenViewContent(
+            name = "Demo User",
+            email = "demo.user@example.com",
+        )
+    }
+}
+
+@PreviewLocales
+@PreviewFontScales
+@PreviewScreenSizes
+@Composable
+private fun ProfileScreenRealUserPreview(
+    @PreviewParameter(ThemeSelectionPreviewParameterProvider::class) theme: ThemeSelection,
+) {
+    SmartHomeTheme(
+        theme = theme,
+    ) {
+        ProfileScreenViewContent(
+            name = "Niels Marsman",
+            email = "niels.marsman@example.com",
+        )
     }
 }
---
+++ shared/feature/profile/src/commonMain/kotlin/network/marsys/smarthome/shared/feature/profile/ProfileViewModel.kt
@@ -0,0 +1,77 @@
+package network.marsys.smarthome.shared.feature.profile
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import network.marsys.smarthome.shared.library.core.coroutines.SuspendingActionStateEffectMutator
+import network.marsys.smarthome.shared.library.core.coroutines.handle
+import network.marsys.smarthome.shared.library.core.coroutines.suspendingActionStateEffectMutator
+import network.marsys.smarthome.shared.library.navigation.NavigationDestination
+import network.marsys.smarthome.shared.library.resources.SmartHomeRes
+import network.marsys.smarthome.shared.library.resources.demo_user
+import network.marsys.smarthome.shared.library.store.ApplicationConfigurationRepository
+import org.jetbrains.compose.resources.getString
+
+internal typealias ProfileStateHolder =
+    SuspendingActionStateEffectMutator<ProfileScreenAction, ProfileScreenState, ProfileScreenEffect>
+
+class ProfileViewModel(
+    private val applicationConfigurationRepository: ApplicationConfigurationRepository,
+    coroutineScope: CoroutineScope,
+) : ViewModel(viewModelScope = coroutineScope),
+    ProfileStateHolder by coroutineScope.suspendingActionStateEffectMutator(
+        state = MutableProfileScreenState(),
+        producer = { state, actions, emitter ->
+            launchUserMutations(
+                state = state,
+                applicationConfigurationRepository = applicationConfigurationRepository,
+            )
+
+            actions.handle(
+                scope = this,
+                keySelector = ProfileScreenAction::key,
+            ) {
+                when (val action = type()) {
+                    ProfileScreenAction.ChangeAppAppearance -> action.flow.collect {
+                        emitter.emit(
+                            effect = ProfileScreenEffect.Navigate(
+                                target = NavigationDestination.ChangeAppAppearanceModal,
+                            ),
+                        )
+                    }
+
+                    ProfileScreenAction.Logout -> Unit
+
+                    ProfileScreenAction.ResetOnboarding -> Unit
+                }
+            }
+        },
+    )
+
+context(scope: CoroutineScope)
+private fun launchUserMutations(
+    state: MutableProfileScreenState,
+    applicationConfigurationRepository: ApplicationConfigurationRepository,
+) {
+    scope.launch {
+        applicationConfigurationRepository.isDemoMode.collect {
+            state.user = when (it) {
+                true -> getString(SmartHomeRes.string.demo_user)
+                else -> "Niels"
+            }
+
+            state.email = when (it) {
+                true -> "demo.user@example.com"
+                else -> "niels.marsman@example.com"
+            }
+        }
+    }
+}
+
+private class MutableProfileScreenState : ProfileScreenState {
+    override var user: String by mutableStateOf("")
+    override var email: String by mutableStateOf("")
+}
