0ca625764bfbcc5f18b19815e5787bf752eef6a6
Niels Marsman <niels.marsman@gmail.com>
2026-07-31 13:42:10 +0200

    SMART-54 - Made use of the style api's for texts

--- shared/feature/zones/build.gradle.kts
+++ shared/feature/zones/build.gradle.kts
@@ -42,7 +42,7 @@ kotlin {
 //
             implementation(libs.compose.foundation)
             implementation(libs.compose.resources)
-//            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.compose.ui.tooling.preview)
 //
 //            implementation(libs.koin.compose.viewmodel)
         }
